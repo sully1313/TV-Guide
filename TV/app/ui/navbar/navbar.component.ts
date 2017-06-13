a395851e650a9367b2588f8c719a55618b37e469
Matt Sullivan <mattbsullivan@gmail.com>
2017-06-12 17:25:48 -0700

    Footer and Nav Components

---
+++ TV/app/ui/navbar/navbar.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavItem } from './navItems';
+
+@Component({
+  moduleId: module.id,
+  selector: 'tv-navbar',
+  templateUrl: 'navbar.template.html'
+})
+
+export class NavbarComponent {
+  appName:string="TV Guide";
+
+  navItems: NavItem[]=[
+    {routerLink:'', label:'Home', active: false}
+  ]
+}
